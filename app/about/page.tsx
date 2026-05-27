0d9d7d6323bf18390958a57dd89a0074547c11c3
Guru Venkataraman <guru@yantrahar.com>
2026-05-27 13:36:03 -0700

    Added vercel analytics

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,15 +3,10 @@
 
 export default function About() { return ( 
     <div className="bg-white text-gray-900">
-      
         <p className="text-lg text-gray-600">
       Ingenious Cloud Solutions builds modern fintech platforms designed
       for real-time commerce and domain-driven payment workflows.
     </p>
-
-    
-
-
     </div>
     
 ) }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import Navbar from '@/components/Navbar'
 import { Footer } from '@/components/Footer'
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata = {
   title: 'Payment Website',
@@ -17,6 +18,7 @@ export default function RootLayout({
     <html lang="en" className="h-full">
       <body className="h-full flex flex-col">
         <Navbar />
+        <Analytics />
 
         <main className="flex-1">
           {children}
